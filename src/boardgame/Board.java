83ff1f51698970964a59635fa66d36be87d00470
Jonas Rocha <127908882+jonasrocha98@users.noreply.github.com>
2024-02-12 21:34:42 -0300

    Possible moves of a piece

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -8,7 +8,7 @@ public class Board {
 	
 	public Board(int rows, int columns) {
 		if (rows <1 || columns < 1) {
-			throw new boardException("Error creating board: There must be at least 1 row and 1 column");
+			throw new BoardException("Error creating board: There must be at least 1 row and 1 column");
 		}
 		this.rows = rows;
 		this.columns = columns;
@@ -25,21 +25,21 @@ public class Board {
 	
 	public Piece piece(int row, int column) {
 		if(!positionExists(row,column)) {
-			throw new boardException("Position not on the board");
+			throw new BoardException("Position not on the board");
 		}
 		return pieces[row][column];
 	}
 	
 	public Piece piece (Position position) {
 		if(!positionExists(position)) {
-			throw new boardException("Position not on the board");
+			throw new BoardException("Position not on the board");
 		}
 		return pieces[position.getRow()][position.getColumn()];
 	}
 	
 	public void placePiece(Piece piece, Position position) {
 		if(thereIsAPiece(position)) {
-			throw new boardException("There is already a piece on position " + position);
+			throw new BoardException("There is already a piece on position " + position);
 		}
 		pieces[position.getRow()][position.getColumn()]=piece;
 		piece.position = position;
@@ -47,7 +47,7 @@ public class Board {
 	
 	public Piece removePiece(Position position) {
 		if(!positionExists(position)) {
-			throw new boardException("position not on the board");
+			throw new BoardException("position not on the board");
 		}
 		if(piece(position) == null) {
 			return null;
@@ -68,7 +68,7 @@ public class Board {
 	
 	public boolean thereIsAPiece(Position position) {
 		if(!positionExists(position)) {
-			throw new boardException("Position not on the board");
+			throw new BoardException("Position not on the board");
 		}
 		return piece(position) !=null;
 	}
